cc9e67d776d55bcdaf641c342df966ae41e2dd36
Tomasz Reizer <t_reizer@yahoo.com>
2019-01-05 17:17:30 +0100

    Added time.js

---
+++ time.js
@@ -0,0 +1,13 @@
+
+
+function time_conv(sec) {
+  var hours = Math.floor(sec/3600);
+  var minutes = Math.floor((sec % 3600) / 60);
+  var seconds = Math.floor((sec % 3600) % 60);
+  //return hours +' h ' + minutes +' min '+ seconds + 'sec';
+  console.log(hours);
+  console.log(minutes);
+  console.log(seconds);
+}
+
+exports.time = time_conv;
